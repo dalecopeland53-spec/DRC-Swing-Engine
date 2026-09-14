f59119817e237676d653ca758e3b7d97b0e73a5f
dalecopeland53-spec <dale.copeland53@gmail.com>
2026-09-15 03:27:09 +1000

    Add DRC anti-glare theme

---
+++ src/theme.js
@@ -0,0 +1,23 @@
+export const C = {
+  bg: '#08131E',
+  bg2: '#0D2233',
+  panel: '#12293B',
+  panel2: '#17374D',
+  line: '#31536A',
+  champagne: '#D7CCB5',
+  champagne2: '#F1E9D8',
+  blue: '#2B80B9',
+  blue2: '#69B7E6',
+  gold: '#D5AE52',
+  text: '#F7F3E9',
+  muted: '#8EA4B3',
+  green: '#7DC38A',
+  red: '#D67272',
+  black: '#02080C'
+};
+
+export const radius = {
+  sm: 8,
+  md: 14,
+  lg: 20
+};
